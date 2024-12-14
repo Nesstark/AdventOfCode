905f65c31749332395b2bf34386771e60960ca39
Emil Madsen <Nesstark0@gmail.com>
2024-12-14 19:35:48 +0100

    Add regex dependency and implement part 2 solution for Day 03

--- Year2024/day03.rs
+++ Year2024/day03.rs
@@ -22,5 +22,29 @@ fn part1(input: &str) {
 }
 
 fn part2(input: &str) {
+    let re = Regex::new(r"(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))").unwrap();
     
+    let mut total_sum = 0;
+    let mut mul_enabled = true;
+    
+    for cap in re.captures_iter(input) {
+        match cap.get(0).map(|m| m.as_str()) {
+            Some("do()") => {
+                mul_enabled = true;
+            },
+            Some("don't()") => {
+                mul_enabled = false;
+            },
+            Some(mul_str) if mul_enabled => {
+                if let Some(num_cap) = re.captures(mul_str) {
+                    let x: i32 = num_cap[2].parse().unwrap();
+                    let y: i32 = num_cap[3].parse().unwrap();
+                    total_sum += x * y;
+                }
+            },
+            _ => {}
+        }
+    }
+    
+    println!("Part 2: Sum of results of enabled multiplications: {}", total_sum);
 }
